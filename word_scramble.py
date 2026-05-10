40db0dd74611f7caece863d614956187a7cb581a
schumjas <schumjas@oregonstate.edu>
2026-05-10 13:08:14 -0700

    completed the assignment

--- word_scramble.py
+++ word_scramble.py
@@ -38,24 +38,45 @@ def scramble_word(secret_word):
 
 # Task 1.1 
 def input_check(secret_word):
-    # Remove 'pass', fill in your code here
-    pass
+    while True:
+        guess = input("Your guess: ")
+        guess2 = ''.join(i for i in guess if i.isalpha())
+        guess2 = guess2.lower()
+        if sorted(guess2) == sorted(secret_word):
+            return guess2
 
 # Task 1.2
 def has_player_won(secret_word, user_guess):
-    # Remove 'pass', fill in your code here
-    pass
+    return secret_word == user_guess
 
 # Task 1.3 
 def get_word_progress(secret_word, user_guess):
-    # Remove 'pass', fill in your code here
-    pass
+    overlap = []
+    for i in range(len(secret_word)):
+        if secret_word[i] == user_guess[i]:
+            overlap.append(secret_word[i])
+        else:
+            overlap.append("*")
+    return ''.join(overlap)
 
 # Task 2.1, 2.2 
 def word_scramble():
+    attempts = 5
     word_list = load_words()
-    # Remove 'pass', fill in your code here
-    pass
+    chosen = choose_word(word_list)
+    print("Welcome to Word Scramble!")
+    scramble_word(chosen)
+    print(f"You have {attempts} attempts to guess the original word.")
+    while attempts > 0:
+        guess = input_check(chosen)
+        if has_player_won(chosen, guess):
+            print("You won!")
+            print(f"You guessed the word with {attempts} attempt(s) remaining.")
+            break
+        else:
+            attempts -= 1
+            print(get_word_progress(chosen, guess))
+    print(f"Sorry, you ran out of guesses. The word was {chosen}.")
 
 
 if __name__ == "__main__":
